c7cb920448cd323c5c86666ccf0c5762e606b0c9
yueyoue <165250587+yueyoue@users.noreply.github.com>
2026-06-07 09:18:22 +0800

    chore: 版本号升级至 11 / 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yueyin"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.1.1"
         vectorDrawables.useSupportLibrary = true
     }
 
